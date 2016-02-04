aab668f675312dfe0bf8112df530aed17d5b1959
Mohammad S. Babaei <info@babaei.net>
2016-02-04 07:58:32 +0330

    fixed the UUID regex

--- Service/Pool.cpp
+++ Service/Pool.cpp
@@ -196,7 +196,7 @@ const int &Pool::StorageStruct::MaxEmailBodyLength() const
 
 const std::string &Pool::StorageStruct::RegexUuid() const
 {
-    static const string regex("/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i");
+    static const string regex("^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$");
     return regex;
 }
 
